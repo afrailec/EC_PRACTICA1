1a13f7c189dae1dd03b31c99fcc8d88071d2a61b
afrailec <76556674+afrailec@users.noreply.github.com>
2021-02-23 12:40:46 +0100

    Add files via upload

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,23 @@
 public class Principal {
     public static void main(String[] args) {
-        Alumno pepe = new Alumno("Pepe", "bb6251", 7.0);
-        pepe.matricularAsignatura("Estructura de Datos");
-        pepe.matricularAsignatura("Algebra");
-        pepe.matricularAsignatura("Etica");
-        pepe.mostrarAlumno();
-        //pepe.mostrarAlumnos();
+        Alumno felipe = new Alumno("Felipe Arias González", "aa1253", 3.50);
+        Alumno manuel = new Alumno("Manuel García Sacedón", "ax0074", 8.35);
+        Alumno margarita = new Alumno("Margarita López Medina", "mj7726", 7.70);
+        Alumno estela = new Alumno("Estela Sánchez Arellano", "bc2658", 6.75);
+
+        felipe.matricularAsignatura("Estructura de Datos");
+        manuel.matricularAsignatura("Estructura de Datos");
+        margarita.matricularAsignatura("Estructura de Datos");
+        estela.matricularAsignatura("Estructura de Datos");
+        estela.matricularAsignatura("Algebra");
+        estela.matricularAsignatura("Estructuras de Computadores");
+
+        System.out.println("---------------------------------------------------");
+        estela.mostrarAsignaturas();
+        System.out.println("---------------------------------------------------");
+        felipe.mostrarAlumno();
+        System.out.println("---------------------------------------------------");
+        estela.mostrarAsignaturas();
+
     }
 }
